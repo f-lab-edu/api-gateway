49157337ff9a97705d7bf8184529cf2f2b85900b
smellhyang <dlgiddk10@gmail.com>
2022-06-19 18:33:51 +0900

    feat/issue2 넷플릭스 유레카(Netflix Eureka) 디스커버리 서버 생성

--- src/main/java/com/moondysmell/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/moondysmell/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.moondysmell.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
