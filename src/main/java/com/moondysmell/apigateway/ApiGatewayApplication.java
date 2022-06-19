eeb31527f4b25e2d7d96ca15b4952d7592d2832f
smellhyang <dlgiddk10@gmail.com>
2022-06-19 22:23:37 +0900

    feat/issue2 넷플릭스 유레카(Netflix Eureka) 디스커버리 서버 삭제 및 client 생성

--- src/main/java/com/moondysmell/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/moondysmell/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,10 @@ package com.moondysmell.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
