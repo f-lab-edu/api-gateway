95b13cefa666a20cf08d45ca87697d3ba8609723
smellhyang <dlgiddk10@gmail.com>
2022-06-22 02:29:08 +0900

    feat/issue5 CircuitBreaker 구성

--- src/main/java/com/moondysmell/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/moondysmell/apigateway/ApiGatewayApplication.java
@@ -4,12 +4,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+//@EnableConfigServer
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
+        //System.setProperty("reactor.netty.http.server.accessLogEnabled","true");
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
+//    @Bean
+//    public ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory(CircuitBreakerRegistry circuitBreakerRegistry) {
+//        ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory = new ReactiveResilience4JCircuitBreakerFactory();
+//        reactiveResilience4JCircuitBreakerFactory.configureCircuitBreakerRegistry(circuitBreakerRegistry);
+//        return reactiveResilience4JCircuitBreakerFactory;
+//    }
+
+//    @Bean
+//    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+//        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+//                .failureRateThreshold(5)
+//                .permittedNumberOfCallsInHalfOpenState(2)
+//                .slidingWindowSize(2)
+//                .minimumNumberOfCalls(5)
+//                .waitDurationInOpenState(Duration.ofMillis(20000))
+//                .build();
+//        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+//                .circuitBreakerConfig(circuitBreakerConfig)
+//                .build());
+//    }
 
 }
