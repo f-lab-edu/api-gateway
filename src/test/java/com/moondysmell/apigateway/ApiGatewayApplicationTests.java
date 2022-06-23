89a593b7460291317a38b8a2cfa5573c33c42c76
dayoungMM <dalbeetm@gmail.com>
2022-06-23 18:25:32 +0900

    feat: JWT decoding - JWT generate 기능은 user 정보(로그인 등)을 담당하는 서비스에서 제공해야함 - 테스트를 위해 ApiGatewayApplicationTests에 generateJwt 메서드를 제공

--- src/test/java/com/moondysmell/apigateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/moondysmell/apigateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,42 @@
 package com.moondysmell.apigateway;
 
+import com.moondysmell.apigateway.auth.JwtProperties;
+import com.moondysmell.apigateway.auth.JwtUtils;
+import com.moondysmell.apigateway.auth.TokenUser;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class ApiGatewayApplicationTests {
+    private JwtUtils jwtUtils;
+
+    @BeforeEach
+    void setUp() {
+        JwtProperties jwtProperties = new JwtProperties();
+        jwtProperties.setSecret("moondysmellwebapplicationsecret");
+        jwtProperties.setExpirationSecond(3600 * 6);
+
+        jwtUtils = new JwtUtils(jwtProperties);
+        jwtUtils.afterPropertiesSet();
+    }
 
     @Test
-    void contextLoads() {
+    void generateJwt() {
+        // given
+        TokenUser tokenUser = new TokenUser("ASk", "ROLE_USER");
+        String generatedToken = jwtUtils.generate(tokenUser);
+
+        // when
+        TokenUser decodedUser = jwtUtils.decode(generatedToken);
+
+        // then
+        log.info("generatedToken : {}", generatedToken);
+        log.info(">>> validation: " +(decodedUser).equals(tokenUser));
+
+
     }
 
 }
